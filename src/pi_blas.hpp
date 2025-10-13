7c40472283ee7ec5a2656d68235f0fd5f65bb6a4
van-artist <shangliu385@gmail.com>
2025-10-13 15:45:33 +0800

    chora:  修改gemm的函数签名

--- src/pi_blas.hpp
+++ src/pi_blas.hpp
@@ -3,4 +3,4 @@
 
 piState piGemm(double *A, double *B, double *C, double alpha, double beta, size_t m, size_t k, size_t n);
 piState piGeMV(double *A, double *B, double *C, double alpha, double beta, size_t m, size_t k);
-piState piSpMV(size_t *column_id, size_t *row_pointer, double *value, size_t m, size_t n);
+piState piSpMV(size_t *A_column_id, size_t *A_row_pointer, double *A_weight, double *x, double *y, size_t m, size_t n, size_t l_x, size_t l_y);
