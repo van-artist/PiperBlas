c7a5e4dc76e1903fb0eee3034004a6f4dd4b8351
van-artist <shangliu385@gmail.com>
2025-12-18 01:13:44 +0800

    feat: 添加CUDA和MKL支持的测试文件，优化性能评估

---
+++ include/core/cuda_test_utils.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cuda_runtime.h>
+#include "core/common.h"
+
+template <class F>
+inline float cuda_time_avg_ms(F launch, int warmup, int iters)
+{
+    cudaEvent_t start, stop;
+    CHECK_CUDA(cudaEventCreate(&start));
+    CHECK_CUDA(cudaEventCreate(&stop));
+
+    for (int i = 0; i < warmup; ++i)
+        launch();
+    CHECK_CUDA(cudaDeviceSynchronize());
+
+    CHECK_CUDA(cudaEventRecord(start));
+    for (int i = 0; i < iters; ++i)
+        launch();
+    CHECK_CUDA(cudaEventRecord(stop));
+    CHECK_CUDA(cudaEventSynchronize(stop));
+
+    float ms = 0.0f;
+    CHECK_CUDA(cudaEventElapsedTime(&ms, start, stop));
+
+    CHECK_CUDA(cudaEventDestroy(start));
+    CHECK_CUDA(cudaEventDestroy(stop));
+
+    return ms / (float)iters;
+}
